58959560428cd056541ca1408aeb64bae2e281d2
manekajal <manekajal2014@gmail.com>
2021-12-01 12:10:55 +0530

    [kajal]:Using Input Onchange Event Binding

--- src/App.js
+++ src/App.js
@@ -8,20 +8,38 @@ class App extends React.Component{
   constructor(){
     super()
     this.state = {
-      title: 'Hello From Bridgelabz'  
+      userName: " " ,
+      nameError: " "
     }
   }
   onClick=($event) =>{
     console.log(" save button is clicked:", $event );
-    window.open(this.url, "_blank")
+    window.open(this.url, "_blank");
   }
-  render(){
-    return (
-      <div>
-        <h1>{this.state.title}</h1>
-        <img src={logo} onClick={this.onClick}
-        alt="The Bridglabz logo : a Bridge to Employment through  lab works " />
+  onNameChange =(event) => {
+    console.log(" value is ", event.target.value);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    this.setState({ userName: event.target.value})
+    if (nameRegex.test(event.target.value)){
+      this.setState({nameError: " "})
+
+    }else{
+      this.setState({nameError: " Name is incorrect"})
+    }
+  }
+  render() {
+    return ( 
+      <>
+        <div>
+          <h1> Hello  {this.state.userName} From Bridglabz </h1>
+          <img src={logo} onClick={this.onClick}
+         alt="The Bridglabz logo : a Bridge to Employment through  lab works " />
       </div>
+      <div>
+        <input onChange={this.onNameChange} />
+        <span className="error-output">{this.state.nameError}</span>
+      </div>
+      </>
     );
   }
 }
