20b3d86036c16f51aff6be587935ad828d81f4b8
manekajal <manekajal2014@gmail.com>
2021-12-01 11:11:29 +0530

    [kajal]: to bridgelabz site on clicking Bridgelabz logo (Event Handler)

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import './App.css';
 import logo from './assets/img/logo.jpg'
 
 class App extends React.Component{
+  url= "https://www.bridgelabz.com/"
   constructor(){
     super()
     this.state = {
       title: 'Hello From Bridgelabz'  
     }
   }
+  onClick=($event) =>{
+    console.log(" save button is clicked:", $event );
+    window.open(this.url, "_blank")
+  }
   render(){
     return (
       <div>
         <h1>{this.state.title}</h1>
-        <img src={logo}
+        <img src={logo} onClick={this.onClick}
         alt="The Bridglabz logo : a Bridge to Employment through  lab works " />
       </div>
     );
